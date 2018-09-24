72d11d535613e08e703f58f8c9d0ac4814ad51a7
Brandon Dooley <dooleyb1@tcd.ie>
2018-09-24 13:03:06 +0100

    added tutorial1 solution & updated slides

---
+++ mt/computer_architecture_3021/assignments/1/t1.h
@@ -0,0 +1,20 @@
+#pragma once
+
+//
+// t1.h
+// Copyright(C) 2018 dooleyb1@tcd.ie
+//
+// example of mixing C++ and IA32 assembly language
+//
+
+//
+// NB: "extern C" to avoid procedure name mangling by compiler
+//
+
+extern "C" int g;			
+
+extern "C" int _cdecl min(int, int, int);  
+extern "C" int _cdecl p(int, int, int, int);
+extern "C" int _cdecl gcd(int, int);
+
+// eof
