e7e8ff936df583f64ad09bcd6eb8ce2479e9147a
dooleyb1 <dooleyb1@tcd.ie>
2018-10-11 10:45:50 +0100

    added online course queries

---
+++ mt/information_management_3041/online_course/1/queries.sql
@@ -0,0 +1,26 @@
+-- Show all customers
+SELECT * FROM Customer
+
+-- Select Mary Hoyne's customer_id
+SELECT customer_id FROM Customer WHERE customer_name = 'Mary Hoyne'
+
+-- Select all flights
+SELECT * FROM Flight
+
+-- Select all from bookings
+SELECT * FROM Booking
+
+-- Select all from fares
+SELECT * FROM Fare
+
+-- Select all of Mary Hoyne's bookings
+SELECT * FROM Booking WHERE customer_id = (SELECT customer_id FROM Customer WHERE customer_name = 'Mary Hoyne')
+
+-- What flight_no is Mary Hoyne on on the date x
+SELECT flight_no FROM Booking WHERE customer_id = (SELECT customer_id FROM Customer WHERE customer_name = 'Mary Hoyne') AND departure_date = '2013-05-21'
+
+-- Select all flights from Dublin -> Heathrow
+SELECT flight_no FROM Flight WHERE departure_airport = 'a1' AND arrival_airport = 'a3'
+
+-- Select the fare of flight from Dublin -> Heathrow
+SELECT fare FROM Fare WHERE flight_no = (SELECT flight_no FROM Flight WHERE departure_airport = 'a1' AND arrival_airport = 'a3')
