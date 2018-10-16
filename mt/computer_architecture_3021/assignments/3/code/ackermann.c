67d62986643247229a1089c390f16738fc7839e1
dooleyb1 <dooleyb1@tcd.ie>
2018-10-16 16:26:59 +0100

    fixed underflows

--- mt/computer_architecture_3021/assignments/3/code/ackermann.c
+++ mt/computer_architecture_3021/assignments/3/code/ackermann.c
@@ -5,25 +5,31 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-#define MAX_WINDOWS 16
-
 // Function declarations
-int ackermann(int x, int y, int depth);
+int ackermann(int x, int y);
 
 // Initialise variables
-int depth = 0;
+int depth = 1;
+int maxWindows = 0;
 int maxDepth = 0;
 int overflowCount = 0;
 int underflowCount = 0;
-int windowsUsed = 0;
+int windowsUsed = 2;
 int callCount = 0;
 
 int main(int argc, char **argv) {
 
 	int x,y;
 
+	if(argc!=4)
+	{
+		printf("\nUsage : %s <number1> <number2> <max_windows>\n",argv[0]);
+		exit(0);
+	}
+
 	x=atoi(argv[1]);
 	y=atoi(argv[2]);
+	maxWindows=atoi(argv[3]);
 
   unsigned long microSeconds = 0;
   float milliSeconds = 0.0;
@@ -39,29 +45,28 @@ int main(int argc, char **argv) {
 
   // Run Ackermann(3,6)
   for(i = 0; i < 1; i++){
-    BS += ackermann(3, 6, 0);
+    BS += ackermann(3, 6);
   }
 
   // Stop clock
   endTime = clock();
 
   // Calculate elapsed time
-  unsigned long elapsedTime = endTime - startTime;
-  elapsedTime = elapsedTime / (i + 1);
+	double elapsed = (double)(endTime - startTime) * 1000.0 / CLOCKS_PER_SEC;
 
-	printf("\nAckermann Function with inputs (%d,%d) is %d\n", x, y, BS);
+	printf("\nAckermann Function with inputs (%d,%d) and maxWindows (%d) is %d\n", x, y, maxWindows, BS);
 	printf("\nFunction called %d times.\n", callCount);
   printf("\nOverflow occurred %d times.\n", overflowCount);
   printf("\nUnderflow occurred %d times.\n", underflowCount);
-  printf("\nMax depth %d.\n", maxDepth);
+  printf("\nMax depth %d\n", maxDepth);
+	printf("\nTime elapsed in ms: %f\n", elapsed);
 
 
   return 0;
 }
 
-int ackermann(int x, int y, int depth) {
+int ackermann(int x, int y) {
 
-  depth++;
   callCount++;
 
   // Check for overflow
@@ -69,32 +74,61 @@ int ackermann(int x, int y, int depth) {
     maxDepth = depth;
   }
 
-  // Check windows for overflows
-  if(windowsUsed == MAX_WINDOWS){
-    overflowCount++;
-  } else{
-    windowsUsed++;
-  }
-
+	// Exit point here
   if (x == 0) {
-
-    depth--;
-
-    // Check windows for underflows
-    if(windowsUsed == 2){
-      underflowCount++;
-    } else{
-      windowsUsed--;
-    }
-
     return y+1;
   }
 
+	// Entry point one
   else if (y == 0) {
-    return ackermann(x-1, 1, depth+1);
+
+		depth++;
+
+		// Check windows for overflows
+	  if(windowsUsed == maxWindows){
+	    overflowCount++;
+	  } else{
+	    windowsUsed++;
+	  }
+
+		// Catch ackermann value first
+    int val = ackermann(x-1, 1);
+
+		depth--;
+
+		// Check windows for underflows
+		if(windowsUsed == 2){
+			underflowCount++;
+		} else{
+			windowsUsed--;
+		}
+
+		return val;
   }
 
+	// Entry point two
   else {
-    return ackermann(x-1, ackermann(x, y-1, depth+1), depth+1);
+
+		depth++;
+
+		// Check windows for overflows
+	  if(windowsUsed == maxWindows){
+	    overflowCount++;
+	  } else{
+	    windowsUsed++;
+	  }
+
+	  int val = ackermann(x-1, ackermann(x, y-1));
+
+		depth--;
+
+		// Check windows for underflows
+		if(windowsUsed == 2){
+			underflowCount++;
+		} else{
+			windowsUsed--;
+		}
+
+		return val;
   }
 }
--- mt/computer_architecture_3021/assignments/3/code/t3-optimised.asm
+++ mt/computer_architecture_3021/assignments/3/code/t3-optimised.asm
@@ -32,14 +32,12 @@ min2:
 p:
       add r9, r0, r10 							;	r10 = g
       add r26, r0, r11 							; r11 = i
-      add r27, r0, r12 							; r12 = j
       callr r25, min 								; min(g, i, j)
-      xor r0, r0, r0 								; reset nop delay
+      add r27, r0, r12 							; r12 = j
       add r1, r0, r10 							; r10 = min(g, i, j)
       add r28, r0, r11 							; r11 = k
-      add r29, r0, r12 							; r12 = l
       callr r25, min 								; min(min(g, i, j), k, l)
-      xor r0, r0, r0 								; nop reset
+      add r29, r0, r12 							; r12 = l
       ret r25, 0 										; return(0)
       xor r0, r0, r0 								; nop reset
 
@@ -56,20 +54,18 @@ gcd:
 			je gcd_retA
 
 			add r26, r0, r10 							; r10 = a
-			add r27, r0, r11 							; r11 = b
 			callr r25, mod								; mod(a, b)
-			xor r0, r0, r0 								; nop reset
+			add r27, r0, r11 							; r11 = b
 
 			add r27, r0, r10							; r10 = b
 			add r1, r0, r11 							; r11 = mod(a,b)
 			callr r25, gcd								; gcd(b, (a%b))
+      add r1, r0, r11 							; r11 = mod(a,b)
 
-			xor r0, r0, r0 								; nop result
 			ret r25, 0 										; return(0)
 			xor r0, r0, r0 								; nop reset
 
 gcd_retA:
-			xor r0, r0, r0 								; nop in delay slot
 			add r26, r0, r25 							; r25 = a
 			ret r25, 0										; return(0)
 			xor r0, r0, r0								; nop reset
