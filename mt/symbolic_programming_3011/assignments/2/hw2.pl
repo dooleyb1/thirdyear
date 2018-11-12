07afd07fab573d287da1d7e8691cf4c1e9f9980b
dooleyb1 <dooleyb1@tcd.ie>
2018-11-12 14:49:16 +0000

    added hw2

--- mt/symbolic_programming_3011/assignments/2/hw2.pl
+++ mt/symbolic_programming_3011/assignments/2/hw2.pl
@@ -23,3 +23,54 @@ right(0) --> [].
 right(Count) --> [0], right(Count).
 % If a 1 is found, update count by 2
 right(UpdatedCount) --> [1], right(Count), {UpdatedCount is Count + 2}.
+
+% Write a DCG that outputs strings:
+%
+% [Col1,Nat1,Pet1, Col2,Nat2,Pet2, Col3,Nat3,Pet3]
+%
+% Such that:
+%
+%   Nationalities ->  [english, spanish, japanese]
+%   Pets -> [jaguar, snail, zebra]
+
+% Declare person definition and conditions
+s --> person(Colour1, Nationality1, Pet1), person(Colour2, Nationality2, Pet2), person(Colour3, Nationality3, Pet3),
+        {Colour1 \== Colour2, Colour1 \== Colour3, Colour2 \== Colour3,
+         Nationality1 \== Nationality2, Nationality1 \== Nationality3, Nationality2 \== Nationality3,
+         Pet1 \== Pet2, Pet1 \== Pet3, Pet2 \== Pet3}.
+
+person(Colour, Nationality, Pet) --> colour(Colour), nationality(Nationality), pet(Pet).
+
+% Colour options
+colour('red')   --> [red].
+colour('blue')  --> [blue].
+colour('green') --> [green].
+
+% Nationality options
+nationality('english')  --> [english].
+nationality('spanish')  --> [spanish].
+nationality('japanese') --> [japanese].
+
+% Pet options
+pet('jaguar') --> [jaguar].
+pet('snail')  --> [snail].
+pet('zebra')  --> [zebra].
+
+% Write a DCG that given a non-negative integer Sum, accepts lists of integers
+%  ≥ 1 that add up to Sum. For example:
+%
+%   | ?- s(3,L,[]).
+%   L = [3];
+%   L = [2,1];
+%   L = [1,2];
+%   L = [1,1,1];
+% no
+
+% mkList predicate that makes list of descending numbers from N to 1
+mkList(0, []).
+mkList(N, [N|T]) :- N > 0, X is N-1, mkList(X, T).
+
+% Sum of 0 is empty list
+s(0) --> [].
+% Sum to N
+s(N) --> {mkList(N, DescList), member(Item, DescList), Remainder is N-Item}, [Item], s(Remainder).
