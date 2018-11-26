2066da0f0f0fdd3aaaea1384fe8068ef652781f7
dooleyb1 <dooleyb1@tcd.ie>
2018-11-26 11:59:11 +0000

    symbolic exam study

--- exams/symbolic_programming_3011/useful.pl
+++ exams/symbolic_programming_3011/useful.pl
@@ -28,3 +28,22 @@ reverse([H|T], R) :- reverse(T, RT), append(RT, [H], R).
 % Reverse a list - with accumulator
 accReverse([], L, L).
 accReverse([H|T], Acc, Rev) :- accReverse(T, [H|Acc], Rev).
+
+% Member of a list
+member(X, [H|T]) :- X == H; member(X, T).
+
+% Not a member of a list
+nonmember(_, []).
+nonmember(X, [H|T]) :- X \= H, nonmember(X, T).
+
+% Member of L1 but not L2
+diff(X, L1, L2) :- member(X, L1), nonmember(X, L2).
+
+% Write a DCG for the set of strings a^n b^n+m c^m
+s -> ap, bp.
+
+ap -> [].
+ap -> [a], ap, [b].
+
+bp -> [].
+bp -> [b], bp, [c].
