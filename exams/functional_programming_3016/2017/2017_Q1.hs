e7a3d84a1715c7ff1504fdb030c8d914b67e0476
dooleyb1 <dooleyb1@tcd.ie>
2018-11-29 16:03:13 +0000

    functional study

---
+++ exams/functional_programming_3016/2017/2017_Q1.hs
@@ -0,0 +1,46 @@
+-- Q1
+
+-- a)
+
+tail' :: [a] -> [a]
+tail' []     = error "empty list"
+tail' (x:xs) = xs
+
+-- b)
+
+last' :: [a] -> a
+last' []       = error "empty list"
+last' (x:y:[]) = y
+last' (x:xs)   = last' xs
+
+-- c)
+
+init' :: [a] -> [a]
+init' [] = error "empty list"
+init' (x:y:[]) = x:[]
+init' (x:xs) = x : init' xs
+
+-- d)
+
+splitAt' :: Int -> [a] -> ([a], [a])
+splitAt' 0 ys = ([], ys)
+splitAt' _ [] = ([], [])
+splitAt' n (y:ys)
+  | n < 0     = ([], (y:ys))
+  | otherwise = ((y:a), b)
+  where (a,b) = splitAt' (n-1) ys
+
+-- e)
+
+replicate' :: Int -> a -> [a]
+replicate' 0 _  = []
+replicate' n x  = x : replicate (n-1) x
+
+-- f)
+
+foldl1' :: (a -> a -> a) -> [a] -> a
+foldl1' f (x:xs) = foldl' f x xs
+
+foldl' :: (a -> a -> a) -> a -> [a] -> a
+foldl' _ z []     = z
+foldl' f z (x:xs) = foldl' f (f z x) xs
---
+++ exams/functional_programming_3016/2017/2017_Q2.hs
@@ -0,0 +1,28 @@
+-- Q2
+
+-- a)
+
+hof :: a -> (a -> a -> a)  -> [a] -> a
+hof x f []      = x
+hof x f (y:ys)  = f y (hof x f ys)
+
+
+f1 :: (Num a) => [a] -> a
+f1 = hof 42 (*)
+
+f2 :: (Num a) => [a] -> a
+f2 = hof 0 g
+  where g x y = (99*y)
+
+f3 :: (Num a) => [a] -> a
+f3 = hof 0 (+)
+
+f4 :: (Num a) => [a] -> [a]
+f4 = hof [] (++)
+
+f5 :: (Num a) => [a] -> a
+f5 = hof 0 g
+  where g x y = (x-42) + y
+
+f5' [] = 0
+f5' (x:xs) = (x-42) + f5' xs
