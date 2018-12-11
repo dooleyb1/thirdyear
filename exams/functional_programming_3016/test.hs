8488f15b4b6c909be10fa75ae0781513263f28a7
dooleyb1 <dooleyb1@tcd.ie>
2018-12-11 19:31:07 +0000

    hof

--- exams/functional_programming_3016/test.hs
+++ exams/functional_programming_3016/test.hs
@@ -1,5 +1,23 @@
-dropWhile' :: (a -> Bool) -> [a] -> [a]
-dropWhile' _ xs@[]       =  []
-dropWhile' p xs@(x:xs')
-           | p x         =  xs
-           | otherwise   =  dropWhile' p xs'
+hof :: a -> (a -> b -> a) -> [b] -> a
+hof z _ [] = z
+hof z f (x:xs) = hof (f z x) f xs
+
+f1 :: (Num a) => a -> [a] -> a
+f1 p l = hof p f l
+  where f x y = x*y
+
+f2 :: (Num a) => a -> [a] -> a
+f2 p l = hof p f l
+  where f x y = y+1
+
+f3 :: (Num a) => a -> [a] -> a
+f3 p l = hof p f l
+  where f x y = x+y
+
+f4 :: (Num a) => [a] -> [[a]] -> [a]
+f4 p l = hof p f l
+  where f x y = x ++ y
+
+f5 :: (Num a) => a -> [a] -> a
+f5 p l = hof p f l
+  where f x y = (x+y*y)
