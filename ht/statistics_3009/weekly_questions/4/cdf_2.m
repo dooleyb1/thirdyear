1cff812af3bc34b5177b2879fec5fcf46b6711f9
dooleyb1 <dooleyb1@tcd.ie>
2019-02-28 17:43:26 +0000

    final submission

--- ht/statistics_3009/weekly_questions/4/cdf_2.m
+++ ht/statistics_3009/weekly_questions/4/cdf_2.m
@@ -1,15 +1,12 @@
 x = [1, 2, 3, 4, 5, 6];
 y = [0, 0, 0, 0, 0, 0];
 
-lastResult=0;
-for i=1:5
- px=(i/6)^4;
- newResult= px + lastResult;
- lastResult = newResult;
- y(i) = newResult;
-end
+diff = 6;
 
-y(6) = (6/6)^4;
+for i=1:6
+ y(diff) = 1-((6-diff)/6)^4;
+ diff = diff - 1;
+end
 
 bar(x,y)
 
