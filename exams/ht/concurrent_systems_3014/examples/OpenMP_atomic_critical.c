fdfdfcd2244ffab17bfcbff479dd8d84084fa170
dooleyb1 <dooleyb1@tcd.ie>
2019-04-16 14:52:20 +0100

    updates

--- exams/ht/concurrent_systems_3014/examples/OpenMP_atomic_critical.c
+++ exams/ht/concurrent_systems_3014/examples/OpenMP_atomic_critical.c
@@ -1,7 +1,29 @@
-/* example of requiring all variables be declared shared or non-shared */
-#pragma omp parallel default(none) shared(n,x,y) private(i)
+/*
+ * An atomic update can update a variable in a single unbreakable step
+ *
+ * This code guarantees x will incremement by the number of threads
+*/
+#pragma omp parallel
 {
-	#pragma omp for
-	for (i=0; i<n; i++)
-		x[i] += y[i];
+	#pragma omp atomic
+	x++;
+}
+
+
+/* Only ONE THREAD can be inside a critical section at any one time */
+#pragma omp parallel
+{
+	#pragma omp critical
+	{
+		x++;
+	}
+}
+
+/* All critical sections clash with each other, so naming them is a good idea */
+#pragma omp parallel
+{
+	#pragma omp critical  (update_x)
+	{
+		x++;
+	}
 }
