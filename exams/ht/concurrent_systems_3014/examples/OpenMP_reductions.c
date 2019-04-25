be18beb9e5c35f3f66bab294db12c4eb3354e83b
dooleyb1 <dooleyb1@tcd.ie>
2019-04-25 13:52:41 +0100

    new concurrent

--- exams/ht/concurrent_systems_3014/examples/OpenMP_reductions.c
+++ exams/ht/concurrent_systems_3014/examples/OpenMP_reductions.c
@@ -1,5 +1,6 @@
 /* OpenMP automatically combines the local copies together to create a final value at the end of the parallel section */
 int sum = 0;
+
 #pragma omp parallel for reduction(+:sum)
 {
 	for ( i = 0; i < n; i++ ){
